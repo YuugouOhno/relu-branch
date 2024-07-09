4e801bde8a3624d12ca7e0cc987cfea07120f09f
T08Y02 <pon9q9@gmail.com>
2024-07-09 15:53:28 +0900

    ダークモード対応：暫定

--- katsuraohackathon-2024-lp/app/component/Container.tsx
+++ katsuraohackathon-2024-lp/app/component/Container.tsx
@@ -18,7 +18,7 @@ const Container = ({
     backgroundColor = "transparent",
 }: ContainerProps) => (
 <>
-    <div className={`md:min-h-screen flex flex-col items-center justify-center font-bold ${backgroundColor} md:px-0 px-6`}>
+    <div className={`md:min-h-screen text-black flex flex-col items-center justify-center font-bold ${backgroundColor} md:px-0 px-6`}>
         <div className="z-10 text-center">
             {title && <p className="text-vh-25 -mb-10">{title}</p>}
             {subTitle && <h2 className="text-3xl mb-10">{subTitle}</h2>}
