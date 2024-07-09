0850b71f229291bcc8414d8f95681528ff70e33d
YuugouOhno <yuugou.ohno@gmail.com>
2024-07-09 16:26:13 +0900

    サムネイルの追加

--- katsuraohackathon-2024-lp/app/layout.tsx
+++ katsuraohackathon-2024-lp/app/layout.tsx
@@ -9,6 +9,30 @@ export const metadata: Metadata = {
   description: "福島にある葛尾村という集落を舞台に地域密着型のハッカソンを開催します！村でエビの養殖をしている株式会社HANERU葛尾様での業務体験・ヒアリングから要件定義を行い、ユーザーを意識した実践的な開発経験を積むことができます！",
   keywords: "かつらおハッカソン, 葛尾, 葛尾村, ハッカソン",
   robots: "index, follow",
+  openGraph: {
+    title: "かつらおハッカソン2024",
+    description: "福島にある葛尾村という集落を舞台に地域密着型のハッカソンを開催します！",
+    images: [
+      {
+        url: '/thumbnail.png',
+        width: 800,
+        height: 600,
+        alt: 'サムネイル画像の説明',
+      },
+    ],
+    url: 'https://relu-branch.com/katsurao-hackathon/2024',
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "かつらおハッカソン2024",
+    description: "福島にある葛尾村という集落を舞台に地域密着型のハッカソンを開催します！",
+    images: [
+      {
+        url: '/thumbnail.png',
+        alt: 'サムネイル画像の説明',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
