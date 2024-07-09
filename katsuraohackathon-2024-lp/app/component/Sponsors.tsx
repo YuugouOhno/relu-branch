f9a8739b457edc9c49dbe49c14806fd8d0431a08
YuugouOhno <yuugou.ohno@gmail.com>
2024-07-09 16:06:05 +0900

    イノベ説明

--- katsuraohackathon-2024-lp/app/component/Sponsors.tsx
+++ katsuraohackathon-2024-lp/app/component/Sponsors.tsx
@@ -9,14 +9,10 @@ const Sponsors = () => (
         <Sponsor title="株式会社ORENDA WORLD" logo_url="" hp_url="https://orenda.co.jp/"
         describe="" />
         <Sponsor title="福島イノベ機構" logo_url="" hp_url="https://www.fipo.or.jp/"
-        describe="" />
-        <div className="text-black">
-        本事業は福島イノベーション・コースト構想の採択を受け、<br/>
-        イノベ地域来訪者受入体制構築事業<br/>
-        「地域の価値向上に向けたブラッシュアップ事業の実施」のうち<br/>
-        「かつらおハッカソン」として実施します。
-        </div>
-        <br/><br/><br/>
+        describe="本事業は福島イノベーション・コースト構想の採択を受け、
+        イノベ地域来訪者受入体制構築事業
+        「地域の価値向上に向けたブラッシュアップ事業の実施」のうち
+        「かつらおハッカソン」として実施します。" />
 
     </>
 )
