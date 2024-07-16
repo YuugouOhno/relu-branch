2ac38a8cd33db48a7b7191b61cc5fc1bd75f873b
YuugouOhno <yuugou.ohno@gmail.com>
2024-07-16 14:35:18 +0900

    サムネの表示

--- katsuraohackathon-2024-lp/app/layout.tsx
+++ katsuraohackathon-2024-lp/app/layout.tsx
@@ -7,28 +7,28 @@ export const dynamic = 'force-dynamic'
 
 const inter = Noto_Sans({ subsets: ["latin"] });
 
-// export const metadata: Metadata = {
-//   title: "",
-//   description: "福島にある葛尾村という集落を舞台に地域密着型のハッカソンを開催します！村でエビの養殖をしている株式会社HANERU葛尾様での業務体験・ヒアリングから要件定義を行い、ユーザーを意識した実践的な開発経験を積むことができます！",
-//   keywords: "かつらおハッカソン, 葛尾, 葛尾村, ハッカソン",
-//   robots: "index, follow",
-//   openGraph: {
-//     title: "かつらおハッカソン2024",
-//     description: "福島にある葛尾村という集落を舞台に地域密着型のハッカソンを開催します！",
-//     url: 'https://relu-branch.com/katsurao-hackathon/2024',
-//     siteName: "かつらおハッカソン2024",
-//     locale: 'ja_JP',
-//     type: "website", // 必須プロパティ
-//     images: [
-//       {
-//         url: 'https://katsuraohackathon-2024-b7r2kv0v1-yuugouohnos-projects.vercel.app/opengraph-image.png?d3a7edbcfc4748f1=',  // 相対パス
-//         width: 800,
-//         height: 600,
-//         alt: 'サムネイル画像の説明',
-//       },
-//     ],
-//   },
-// };
+export const metadata: Metadata = {
+  title: "かつらおハッカソン",
+  description: "福島にある葛尾村という集落を舞台に地域密着型のハッカソンを開催します！村でエビの養殖をしている株式会社HANERU葛尾様での業務体験・ヒアリングから要件定義を行い、ユーザーを意識した実践的な開発経験を積むことができます！",
+  keywords: "かつらおハッカソン, 葛尾, 葛尾村, ハッカソン",
+  robots: "index, follow",
+  openGraph: {
+    title: "かつらおハッカソン2024",
+    description: "福島にある葛尾村という集落を舞台に地域密着型のハッカソンを開催します！",
+    url: 'https://relu-branch.com/katsurao-hackathon/2024',
+    siteName: "かつらおハッカソン2024",
+    locale: 'ja_JP',
+    type: "website", // 必須プロパティ
+    images: [
+      {
+        url: 'https://katsuraohackathon-2024-b7r2kv0v1-yuugouohnos-projects.vercel.app/opengraph-image.png?d3a7edbcfc4748f1=',  // 相対パス
+        width: 800,
+        height: 600,
+        alt: 'サムネイル画像の説明',
+      },
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
