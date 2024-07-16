3bdf652e194c31b906821cbdb13c943ab534cd76
YuugouOhno <yuugou.ohno@gmail.com>
2024-07-16 15:00:04 +0900

    サムネの表示

--- katsuraohackathon-2024-lp/app/layout.tsx
+++ katsuraohackathon-2024-lp/app/layout.tsx
@@ -38,10 +38,9 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <Head>
-        <meta name="google-site-verification" content="mgjC376pDDqXAumrXz_j8QTxHQ3hJBXxEz6fMJNfExk" />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width" />
-        <link rel="icon" type="image/svg+xml" href="/favicon.png" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
         <title>かつらおハッカソン2024</title>
         <meta name="description" content="福島にある葛尾村という集落を舞台に地域密着型のハッカソンを開催します！村でエビの養殖をしている株式会社HANERU葛尾様での業務体験・ヒアリングから要件定義を行い、ユーザーを意識した実践的な開発経験を積むことができます！" />
         <meta name="keywords" content="かつらおハッカソン, 葛尾, 葛尾村, ハッカソン" />
@@ -56,6 +55,11 @@ export default function RootLayout({
         <meta property="og:image:width" content="800" />
         <meta property="og:image:height" content="600" />
         <meta property="og:image:alt" content="サムネイル画像" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="かつらおハッカソン2024" />
+        <meta name="twitter:description" content="福島にある葛尾村という集落を舞台に地域密着型のハッカソンを開催します！" />
+        <meta name="twitter:image" content="https://relu-branch.com/katsurao-hackathon/2024/thumbnail.png" />
+        <meta name="google-site-verification" content="mgjC376pDDqXAumrXz_j8QTxHQ3hJBXxEz6fMJNfExk" />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
