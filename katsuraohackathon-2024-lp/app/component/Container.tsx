84e2ae58bb480358b997b79e437fe44aea74dc2f
YuugouOhno <yuugou.ohno@gmail.com>
2024-07-16 16:29:11 +0900

    サムネの表示

--- katsuraohackathon-2024-lp/app/component/Container.tsx
+++ katsuraohackathon-2024-lp/app/component/Container.tsx
@@ -20,7 +20,7 @@ const Container = ({
 <>
     <div className={`md:min-h-screen text-black flex flex-col items-center justify-center font-bold ${backgroundColor} md:px-0 px-6`}>
         <div className="z-10 text-center">
-            {title && <p className="text-vh-25 -mb-10">{title}</p>}
+            {title && <p className="md:text-vh-25 md:mb-16 text-5xl">{title}</p>}
             {subTitle && <h2 className="text-3xl mb-10">{subTitle}</h2>}
         </div>
         <div className="z-10 text-center md:mb-0 mb-12 ">
