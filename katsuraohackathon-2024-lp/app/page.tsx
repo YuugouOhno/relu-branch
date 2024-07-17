9cb74bbe20de45a2e64ac35e078c4380cb714b71
YuugouOhno <yuugou.ohno@gmail.com>
2024-07-17 12:20:17 +0900

    test

--- katsuraohackathon-2024-lp/app/page.tsx
+++ katsuraohackathon-2024-lp/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'; // Add this directive to indicate this is a client component
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import Head from 'next/head'
 
 export default function Home() {
   const router = useRouter();
@@ -9,5 +10,7 @@ export default function Home() {
     router.push('https://relu-branch.com/katsurao-hackathon/2024');
   }, [router]);
 
-  return null; // ページコンテンツは不要なのでnullを返します
+  return (
+    <></>
+  ); // ページコンテンツは不要なのでnullを返します
 }
