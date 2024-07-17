301c4bcea6d3541595c922118f05771f54092b42
YuugouOhno <yuugou.ohno@gmail.com>
2024-07-17 12:22:08 +0900

    test

--- katsuraohackathon-2024-lp/app/katsurao-hackathon/2024/page.tsx
+++ katsuraohackathon-2024-lp/app/katsurao-hackathon/2024/page.tsx
@@ -16,30 +16,6 @@ import FixedButton from '../../component/FixedButton';
 export default function Home() {
   return (
     <>
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width" />
-        <link rel="icon" type="image/png" href="/favicon.png" />
-        <title>かつらおハッカソン2024</title>
-        <meta name="description" content="福島にある葛尾村という集落を舞台に地域密着型のハッカソンを開催します！村でエビの養殖をしている株式会社HANERU葛尾様での業務体験・ヒアリングから要件定義を行い、ユーザーを意識した実践的な開発経験を積むことができます！" />
-        <meta name="keywords" content="かつらおハッカソン, 葛尾, 葛尾村, ハッカソン" />
-        <meta name="robots" content="index, follow" />
-        <meta property="og:title" content="かつらおハッカソン2024" />
-        <meta property="og:description" content="福島にある葛尾村という集落を舞台に地域密着型のハッカソンを開催します！" />
-        <meta property="og:url" content="https://relu-branch.com/katsurao-hackathon/2024" />
-        <meta property="og:site_name" content="かつらおハッカソン2024" />
-        <meta property="og:locale" content="ja_JP" />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://relu-branch.com/katsurao-hackathon/2024/thumbnail.jpg" />
-        <meta property="og:image:width" content="800" />
-        <meta property="og:image:height" content="600" />
-        <meta property="og:image:alt" content="サムネイル画像" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="かつらおハッカソン2024" />
-        <meta name="twitter:description" content="福島にある葛尾村という集落を舞台に地域密着型のハッカソンを開催します！" />
-        <meta name="twitter:image" content="https://relu-branch.com/katsurao-hackathon/2024/thumbnail.jpg" />
-        <meta name="google-site-verification" content="mgjC376pDDqXAumrXz_j8QTxHQ3hJBXxEz6fMJNfExk" />
-      </Head>
       <main>
         <FixedButton/>
         <div className="background-image"></div> {/* 背景画像 */}
--- katsuraohackathon-2024-lp/app/page.tsx
+++ katsuraohackathon-2024-lp/app/page.tsx
@@ -1,16 +1,16 @@
-'use client'; // Add this directive to indicate this is a client component
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import Head from 'next/head'
+// 'use client'; // Add this directive to indicate this is a client component
+// import { useEffect } from 'react';
+// import { useRouter } from 'next/navigation';
+// import Head from 'next/head'
 
-export default function Home() {
-  const router = useRouter();
+// export default function Home() {
+//   const router = useRouter();
 
-  useEffect(() => {
-    router.push('https://relu-branch.com/katsurao-hackathon/2024');
-  }, [router]);
+//   useEffect(() => {
+//     router.push('https://relu-branch.com/katsurao-hackathon/2024');
+//   }, [router]);
 
-  return (
-    <></>
-  ); // ページコンテンツは不要なのでnullを返します
-}
+//   return (
+//     <></>
+//   ); // ページコンテンツは不要なのでnullを返します
+// }
